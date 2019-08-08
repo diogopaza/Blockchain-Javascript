5cd1ca68dc09c1fbb5adbf3cda3642bde60b91a3
diogo paza <diogopazacvel@gmail.com>
2019-08-07 21:20:13 -0300

    imutavel

--- blockchain.js
+++ blockchain.js
@@ -26,18 +26,28 @@ class Blockchain{
     isValid(){
         //percorre todos os blocos começando pelo segundo bloco 
         for(let i=1;i<this.blocks.length;i++){
-            const currentBlock = this.blocks[i]
+             const currentBlock = this.blocks[i]
             const previusBlock = this.blocks[i-1]
-            console.log("funcao Isvalid: ", currentBlock)  
-            
-            if(currentBlock.hash == currentBlock.generateHash()){
-                console.log("current hash", currentBlock.hash)
-                console.log("previus hash", currentBlock.generateHash())
+                        
+            if(currentBlock.hash != currentBlock.generateHash()){
+                console.log("HASH nao corresponde")
+                console.log("Bloco "+currentBlock.data + " INVALIDO" )
                 return false
-
             }
+            if(currentBlock.index != previusBlock.index + 1){
+                console.log("Index nao corresponde")
+                console.log("Bloco "+currentBlock.data + " INVALIDO" )
+                return false
+            }
+            if(currentBlock.previusHash != previusBlock.hash){
+                console.log("Previus Hash nao corresponde")
+                console.log("Bloco "+currentBlock.data + " INVALIDO" )
+                return false
+            }
+            console.log("Bloco "+currentBlock.data + " OK" )
+            
         }
-        
+        return true
     }
 
 }
--- teste.js
+++ teste.js
@@ -1,9 +1,10 @@
 const Block = require('./blockchain')
 
+//testando se o blockchain e imutavel
 const blockchain = new Block()
 blockchain.addBlock("teste")
-blockchain.addBlock("teste")
-blockchain.addBlock("teste")
-blockchain.isValid()
-//console.log(blockchain)
-//console.log(blockchain.isValid())
+blockchain.addBlock("teste1")
+blockchain.addBlock("teste2")
+console.log(blockchain.isValid())//retorna true
+blockchain.blocks[2].index = 10 //altera integrida do bloco 2
+console.log(blockchain.isValid())//retorna false
